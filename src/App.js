4bc05c1a7492c53b36b37990b529f8f1ae88459a
kennytran275 <kennyt275@gmail.com>
2021-04-22 12:55:20 -0400

    changed title

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>WEB 41</h1>
       <h2>
         Deployed with{' '}
         <a
@@ -48,8 +39,6 @@ function App() {
         .
       </p>
       <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
